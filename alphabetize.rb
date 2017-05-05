649e2242635b232f5d48161b7c88f047b5938ff2
coreen <coreeny@cs.washington.edu>
2017-05-04 22:27:17 -0700

    convert alphabetizer to take input/output file paths as command-line args to script

--- alphabetize.rb
+++ alphabetize.rb
@@ -1,14 +1,29 @@
+#!/usr/bin/env ruby
+
 # Coreen Yuen
 # Alphabetizes json file such that keys are in sorted order.
 
+# Usage: ./alphabetize.rb <inputFilepath> <outputFilepath>
+
 require 'json'
+
+supported_file_extensions = Array['.json']
+
+input_filepath = ARGV[0]
+output_filepath = ARGV[1]
+
+# Check that file extensions are .json only
+unless supported_file_extensions.include? File.extname(input_filepath) and supported_file_extensions.include? File.extname(output_filepath)
+	puts 'ERROR: only supported file extensions are: ' + supported_file_extensions.join(', ')
+	exit(false)
+end
 
 input_file = File.read('input.json')
 data_hash = JSON.parse(input_file)
 
 # Write sorted result to another file
 output_file = File.new('actual.json', 'w')
-# using double quotes to evaluate special characters
+# Using double quotes to evaluate special characters
 output_file.write("{\n")
 data_hash.keys.sort.each do |key|
 	val = data_hash[key]
@@ -16,3 +31,5 @@ data_hash.keys.sort.each do |key|
 end
 output_file.write("}")
 output_file.close
+
+exit(true)
