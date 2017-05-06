427d14733cf55b608343d92428e3486a22fa7776
coreen <coreeny@cs.washington.edu>
2017-05-05 22:04:37 -0700

    check 2 command-line args and remove trailing comma from output file json

--- alphabetize.rb
+++ alphabetize.rb
@@ -9,6 +9,13 @@ require 'json'
 
 supported_file_extensions = Array['.json']
 
+# Check that 2 input arguments are provided
+if ARGV.length < 2
+	puts 'ERROR: 2 input arguments are required to execute script'
+	puts "\nUsage: ./alphabetize.rb <inputFilepath> <outputFilepath>"
+	exit(false)
+end
+
 input_filepath = ARGV[0]
 output_filepath = ARGV[1]
 
@@ -25,11 +32,19 @@ data_hash = JSON.parse(input_file)
 output_file = File.new(output_filepath, 'w')
 # Using double quotes to evaluate special characters
 output_file.write("{\n")
-data_hash.keys.sort.each do |key|
-	val = data_hash[key]
-	output_file.write("\t\"" + key + "\": " + val + "\",\n")
+sorted_data_hash = data_hash.sort
+# fencepost
+if sorted_data_hash.length >= 1
+	puts sorted_data_hash.first
+	key, val = sorted_data_hash.first
+	output_file.write("\t\"" + key + "\": " + val + "\"")
 end
-output_file.write("}")
+sorted_data_hash.shift
+sorted_data_hash.each do |key, val|
+	val = data_hash[key]
+	output_file.write(",\n\t\"" + key + "\": " + val + "\"")
+end
+output_file.write("\n}")
 output_file.close
 
 exit(true)
